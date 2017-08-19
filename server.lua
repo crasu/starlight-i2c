13b3828bba16dfc5f9440d0b21bdd191e9887ac8
Christian Kraemer <christian.kraemer@tngtech.com>
2017-08-20 00:15:21 +0200

    add fading

--- server.lua
+++ server.lua
@@ -1,5 +1,40 @@
 local ada_pwm = require("ada_pwm")
 
+local fade_list = {}
+local current_value = {[0] = 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+function fader()
+    print("Fading")
+    for key, entry in pairs(fade_list) do
+        local port = entry["port"]
+        local final_dim = entry["dim"]
+        local dim = 0
+        if math.abs(current_value[port] - final_dim) <= speed then
+            dim = final_dim
+            table.remove(fade_list, key)
+            print("Done with fading for " ... key)
+        else
+            dim = current_value[port] + speed
+        end
+        current_value[port] = dim
+        ada_pwm.led_dim(port, dim)
+    end
+end
+
+function calc_fade_speed(port, dim, fade)
+    local speed = 0
+    if fade == 0 then
+        speed = dim - current_value[port]
+    else
+        speed = math.floor((dim - current_value[port]) / fade) -- maybe we can use // here?
+
+        if speed >= -1 and speed <= 1 then
+            speed = current_value[port] - dim
+        end
+    end
+    return speed
+end
+
 function init()
     local config = require("config")
 
@@ -13,20 +48,30 @@ function init()
     ada_pwm.init_i2c()
     ada_pwm.init_pca()
 
+    local FADER_MS = 500
+
+    tmr.alarm(2, FADER_MS, tmr.ALARM_AUTO, fader)
+
     print(wifi.sta.getip())
 end
 
-init()
-
-srv=net.createServer(net.TCP)
-srv:listen(80, function(conn)
-    conn:on("receive", function(sck, payload)
-        local code, method, port, dim, fade = require("connection").handle(sck, payload)
-        if code == 200 and method == "GET" and port and dim and fade then
-            print("dimming port " .. port .. " to " .. dim " in " ... fade ... " cs")
-            ada_pwm.led_dim(port, dim)
-        end
+function server()
+    srv=net.createServer(net.TCP)
+    srv:listen(80, function(conn)
+        conn:on("receive", function(sck, payload)
+            local code, method, port, dim, fade = require("connection").handle(sck, payload)
+            if code == 200 and method == "GET" and port and dim and fade then
+                print("dimming port " .. port .. " to " .. dim " in " ... fade ... " cs")
+                local speed = calc_fade_speed(port, dim, fade)
+                print("dim speed is " .. speed)
+                local entry = { port = port, dim = dim, speed = speed }
+                table.insert(fade_list, entry)
+            end
+        end)
+        conn:on("sent", function(sck) sck:close() end)
     end)
-    conn:on("sent", function(sck) sck:close() end)
-end)
+end
+
+init()
+server()
 
